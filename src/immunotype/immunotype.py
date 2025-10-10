09c6bcecc287ed80483ccdf824c81f159da00cf2
matteopilz <matteo.pilz@uni-tuebingen.de>
2025-10-10 09:04:38 +0200

    pre merge with main

--- src/immunotype/immunotype.py
+++ src/immunotype/immunotype.py
@@ -24,12 +24,27 @@ mhc_features = None
 lookup_db = None
 
 
-def predict_lookup(peptide_df: pd.DataFrame, selected_alleles: pd.Series) -> pd.DataFrame:
+def predict_lookup(
+    peptide_df: pd.DataFrame, selected_alleles: pd.Series
+) -> pd.DataFrame:
     """Predict binding probabilities using the lookup method."""
 
-    lookup_score_df = pd.merge(lookup_db.loc[lookup_db["allele"].isin(selected_alleles)], peptide_df, how="inner")
-    # Count occurrences of each (sample, locus, allele) combination
-    lookup_score_df = lookup_score_df[["sample", "locus", "allele"]].value_counts().reset_index()
+    lookup_score_df = pd.merge(
+        lookup_db.loc[lookup_db["allele"].isin(selected_alleles)],
+        peptide_df,
+        how="inner",
+    )
+    index = pd.MultiIndex.from_tuples(
+        [
+            [s, allele[4], allele]
+            for s in peptide_df["sample"].unique()
+            for allele in selected_alleles
+        ],
+        names=["sample", "locus", "allele"],
+    )
+    lookup_score_df = (
+        lookup_score_df[["sample", "locus", "allele"]].value_counts().reset_index()
+    )
     # Apply cube root transformation to counts to improve homozygous detection
     lookup_score_df["probability"] = np.cbrt(lookup_score_df["count"])
     lookup_score_df["probability"] = lookup_score_df.groupby(["sample", "locus"])["probability"].transform(lambda x: x / x.max())
@@ -45,13 +60,15 @@ def predict_lookup(peptide_df: pd.DataFrame, selected_alleles: pd.Series) -> pd.
         .sort_index()
     )
 
-    # Check if second highest score is below lookup threshold to call homozygous
-    for sample in lookup_score_df.index.get_level_values(0).unique():
+    for s in lookup_score_df.index.get_level_values(0).unique():
         for p in PLACEHOLDERS:
             if p in selected_alleles:
                 locus = p[4]
-                scores = lookup_score_df.loc[sample, locus].loc[:, "count"]
-                lookup_score_df.loc[sample, locus, p] = ( scores.nlargest(2).values[-1] <= scores.max() * LOOKUP_HOMOZYGOUS_THRESHOLDS[locus]) * 1
+                scores = lookup_score_df.loc[s, locus].loc[:, "count"]
+                lookup_score_df.loc[s, locus, p] = (
+                    scores.nlargest(2).values[-1]
+                    <= scores.max() * LOOKUP_HOMOZYGOUS_THRESHOLDS[locus]
+                ) * 1
 
     lookup_score_df = lookup_score_df.reset_index().drop("count", axis=1)
     return lookup_score_df
@@ -80,23 +97,32 @@ def predict_model(peptide_df: pd.DataFrame, selected_alleles: pd.Series, batch_s
     model.eval()
     with torch.no_grad():
         for batch in tqdm(dataloader, desc="🤖 Running HLA typing prediction..."):
-            predictions.append(np.reshape(model(batch).cpu().detach().numpy(), (len(batch.sample), -1)))
+            predictions.append(
+                np.reshape(model(batch).cpu().detach().numpy(), (len(batch.sample), -1))
+            )
             samples.append(batch.sample)
 
-    probabilities_df = \
-        pd.concat([
+    probabilities_df = pd.concat(
+        [
             pd.DataFrame(np.concatenate(samples), columns=["sample"]),
             pd.DataFrame(np.concatenate(predictions), columns=selected_alleles),
-        ], axis=1) \
-        .melt(id_vars="sample", var_name="allele", value_name="probability")
+        ],
+        axis=1,
+    ).melt(id_vars="sample", var_name="allele", value_name="probability")
 
-    probabilities_df = probabilities_df.groupby(["sample", "allele"])["probability"].mean().reset_index()
+    probabilities_df = (
+        probabilities_df.groupby(["sample", "allele"])["probability"]
+        .mean()
+        .reset_index()
+    )
+
     probabilities_df["locus"] = probabilities_df["allele"].str[4]
-
     return probabilities_df
 
 
-def prepare_data(use_gnn: bool = True, use_lookup: bool = True, gnn_weight_path=None) -> None:
+def prepare_data(
+    use_gnn: bool = True, use_lookup: bool = True, gnn_weight_path=None
+) -> None:
     """Prepare data and load model weights."""
 
     if gnn_weight_path is None:
@@ -152,15 +178,24 @@ def predict(
     if not use_gnn and not use_lookup:
         raise ValueError("Must use GNN or lookup or both")
 
+    if gnn_weight_path is None:
+        gnn_weight_path = PACKAGE_ROOT / "weights" / "gnn_model_weights.pt"
+
     # Load model and lookup
-    prepare_data(use_gnn, use_lookup, gnn_weight_path)
+    if (use_gnn and model is None) or (use_lookup and lookup_db is None):
+        prepare_data(use_gnn, use_lookup, gnn_weight_path)
 
     # Get predictions
     probabilities = {}
+
     if use_gnn:
-        probabilities["model"] = predict_model(peptide_df, selected_alleles, batch_size, max_n_peptides)
+        probabilities["model"] = predict_model(
+            peptide_df, selected_alleles, batch_size, max_n_peptides
+        )
+
     if use_lookup:
         probabilities["lookup"] = predict_lookup(peptide_df, selected_alleles)
+
     if use_gnn and use_lookup:
         pred_df = pd.merge(
             probabilities["model"],
@@ -175,24 +210,37 @@ def predict(
             lambda x: (
                 x["probability_model"] * (w := ENSEMBLE_MODEL_WEIGHTS[x["locus"]])
                 + x["probability_lookup"] * (1 - w)
-            ),axis=1)
+            ),
+            axis=1,
+        )
 
-        pred_df = pred_df[["sample","locus","allele","probability_model","probability_lookup","probability"]]
-
+        pred_df = pred_df[
+            [
+                "sample",
+                "locus",
+                "allele",
+                "probability_model",
+                "probability_lookup",
+                "probability",
+            ]
+        ]
     else:
         pred_df = list(probabilities.values())[0]
         pred_df = pred_df[["sample", "locus", "allele", "probability"]]
 
     pred_df = pred_df.sort_values(["sample", "locus", "allele"])
 
-    # Obtain typing based on highest probably 2 alleles per locus
-    typing = pred_df.loc[pred_df["probability"] > 0]
-    if len(typing) >= 3:
-        typing = (typing.groupby(["sample", "locus"])
-                    .apply(
-                        lambda x: x.sort_values(by="probability")["allele"].iloc[-2:],
-                        include_groups=False)
-                    .reset_index())
+    # typing
+    typing_df = pred_df.loc[pred_df["probability"] > 0]
+    if len(typing_df) >= 3:
+        typing_df = (
+            typing_df.groupby(["sample", "locus"])
+            .apply(
+                lambda x: x.sort_values(by="probability")["allele"].iloc[-2:],
+                include_groups=False,
+            )
+            .reset_index()
+        )
     else:
         # Create empty typing DataFrame with correct columns
         typing_df = pd.DataFrame(columns=["sample", "locus", "allele"])
@@ -200,7 +248,7 @@ def predict(
             f"Missing typing results, possibly due to insufficient input data. Use GNN or increase input size. ",
             stacklevel=2,
         )
-    # filter out homozygous placeholder alleles
-    typing = typing.loc[~typing["allele"].str.contains("homozygous")]
 
-    return pred_df, typing
+    # filter out homozygous placeholder alleles
+    typing_df = typing_df.loc[~typing_df["allele"].str.contains("homozygous")]
+    return pred_df, typing_df
