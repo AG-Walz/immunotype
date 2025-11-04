c73e78c14b9ae21ba241edad046e24ad2cd34650
matteopilz <matteo.pilz@uni-tuebingen.de>
2025-11-04 13:47:22 +0100

    extended app help section

--- src/immunotype/constants.py
+++ src/immunotype/constants.py
@@ -68,22 +68,37 @@ APP_HELP_SECTION = """
 ## 📚 Tutorial and Resources
 
 ### Usage
-1. **Peptides input**: Enter peptide sequences separated by newlines, or upload a file
-2. **Select typing method**: Choose between automatic typing or input known HLA alleles
-4. **Submit**: Click submit to run the prediction
-5. **View results**: See predicted typing and download detailed probabilities
+1. **Peptides input**: Enter peptide sequences separated by newlines, or upload a file.
+2. **Submit**: Click submit to run the prediction.
+3. **View results**: See predicted typing and download detailed probabilities.
 
 ### Input Formats
 #### Peptides
-- **One sample**: One peptide sequence per line (e.g., `ALDGRETD`)
-- **Multiple samples**: Header with sample & peptide, then sample id and peptide sequence
-- **HLA alleles**: One allele per line (e.g., HLA-A*24-27)
-- **Files**: TSV/CSV files with peptide sequences in the first column
+- **One sample**: One peptide sequence per line (e.g., `ALDGRETD`).
+- **Multiple samples**: Header with sample & peptide, then sample id and peptide sequence.
+- **HLA alleles**: One allele per line (e.g., HLA-A*24-27).
+- **Files**: TSV/CSV files with either peptide sequences only or with: sample, peptide as columns.
 
 ### Output
-- **Typing**: Predicted HLA alleles for your sample
-- **Probabilities**: Detailed probability scores for all tested alleles
-- **CSV Download**: Full results table for further analysis
+- **Typing**: Predicted HLA alleles for your sample.
+- **Probabilities**: Detailed probability scores for all tested alleles.
+- **TSV Download**: Full results table for further analysis.
+
+### Additional Settings
+- **Select which model to use**: You can choose between ensemble or GNN and lookup only.
+- **HLA allele input**: Choose the alleles included in the prediction (it is not recommended to change this, see below).¹
+- **Maximum number of peptides**: Change the maximum number of peptides that should be predicted simultaneously.²
+- **Batch size**: Number of samples that should be predicted simultaneously.³
+- **Use GPU**: Run prediction on GPU, will throw a warning if activated and no GPU is available.
+
+### Remarks
+¹ It is not recommended to change the selected alleles, as there is a high probability it will influence the prediction accuracy.\n
+² The maximum number of peptides should be as high as possible and ideally larger than the number of peptides in each sample.
+Together with the batch size, this setting can be used to prevent memory overflow, especially on GPU.\n
+³ In most cases and especially on CPU, the batch size can be increased to speed up the prediction.
+When running on GPU, it is advised to keep the batch size lower.\n
+\n
+Maximum number of peptides, batch size and usage of GPU do not affect a lookup only prediction.
 
 ### Citation
 If you use immunotype in your research, please cite TODO.
